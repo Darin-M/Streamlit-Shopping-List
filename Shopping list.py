2342a8cf45b08364bd91ff3c498644fe2bb7cd04
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-03 19:19:23 +0000

    Update Shopping list.py

--- Shopping list.py
+++ Shopping list.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_excel("https://1drv.ms/x/c/84b14527e3f20dbd/EbsQ-kL-xUhLhDzsnTcE8DQBl16BrzKij7Oq1_wzdoey8Q?e=yxwwjz", sheet_name="Alusvia")
+df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia")
 
 st.write("Welcome to Shopping List!")
+
 
 st.dataframe(df)
