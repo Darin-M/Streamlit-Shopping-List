36194f3fedc6f19841115058024353be8951227b
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-03 21:19:55 +0000

    google sheets?

--- Shopping list.py
+++ Shopping list.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 @st.cache_data
 def main():
-    df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:J")
+    #df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:J")
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
+    df = conn.read(
+        worksheet = "Alusvia",
+        usecols=[1, 9],
+        nrows=3,
+    )    
     st.write("Welcome to Shopping List!")
 
     st.dataframe(df, row_height=80)
