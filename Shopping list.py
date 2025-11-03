4321feef1954cdbae30b668381004eb07f7969cc
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-03 21:33:48 +0000

    revert to local

--- Shopping list.py
+++ Shopping list.py
@@ -4,14 +4,10 @@ from streamlit_gsheets import GSheetsConnection
 
 @st.cache_data
 def main():
-    #df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:J")
-    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:J")
+    #conn = st.connection("gsheets", type=GSheetsConnection)
 
-    df = conn.read(
-        worksheet = "Alusvia",
-        usecols=[1, 9],
-        nrows=3,
-    )    
+    #df = conn.read(worksheet = "Alusvia",usecols=[1, 9],nrows=3,)    
     st.write("Welcome to Shopping List!")
 
     st.dataframe(df, row_height=80)
