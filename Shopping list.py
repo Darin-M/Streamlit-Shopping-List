1f03bfbe42fbde9c5a1b311a0f0e98791c4bdacb
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-03 19:41:55 +0000

    further fixed sheet reading

--- Shopping list.py
+++ Shopping list.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 2, usecols="B:I")
+df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:I")
 
 st.write("Welcome to Shopping List!")
 
