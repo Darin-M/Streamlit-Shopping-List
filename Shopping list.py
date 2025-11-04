8437e10bbf0d5581ceaa5ed6c67c0f704026dfd8
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-04 09:56:28 +0000

    Updated size and Added link to root table

--- Shopping list.py
+++ Shopping list.py
@@ -4,6 +4,8 @@ from streamlit_gsheets import GSheetsConnection
 
 @st.cache_data
 def main():
+    st.set_page_config(layout="wide",
+                       menu_items={"Excel Sheet": "https://1drv.ms/x/c/84b14527e3f20dbd/EbsQ-kL-xUhLhDzsnTcE8DQBl16BrzKij7Oq1_wzdoey8Q?e=emMLTV"})
     df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Alusvia", header = 1, usecols="B:J")
     #conn = st.connection("gsheets", type=GSheetsConnection)
 
