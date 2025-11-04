39ad4e1633c2eeea6dc61c46a191b241f3a9ffa0
Darin-M <101464916+Darin-M@users.noreply.github.com>
2025-11-04 12:59:26 +0000

    Added materias page

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ def main():
     page_title="Home")
     st.title("Wilder Companion") 
     st.write("Welcome to the Wilder Companion app! Here you wll be able to find a range of handy tools for your d&d game to help make things a little bit easier!")
-
+    st.write("For now, press the arrow in the top left corner and explore the pages!")
     st.sidebar.success("Select a page above.")
 
 if __name__ == "__main__":
---
+++ pages/2_Special_Materials.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+@st.cache_data
+def main():
+    st.set_page_config(layout="wide",
+                       page_title="Special Materials")
+                       
+    df = pd.read_excel("Wilder at Catalogue.xlsx", sheet_name="Special Materials", header = 2, usecols="B:I")
+    #conn = st.connection("gsheets", type=GSheetsConnection)
+    st.title("Alusvia Catalogue") 
+    #df = conn.read(worksheet = "Alusvia",usecols=[1, 9],nrows=3,)    
+    st.write("Welcome to the Shopping List!")
+
+    st.dataframe(df, row_height=80, hide_index=True)
+
+if __name__ == "__main__":
+    main()
